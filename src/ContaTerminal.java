a26a4deb30e61f973ee2af442dc47730a9edb6d3
Edton Lemos <edh.15@hotmail.com>
2025-03-20 23:29:36 -0300

    Inserindo o código do ContaTerminal para desafio do curso

---
+++ src/ContaTerminal.java
@@ -0,0 +1,96 @@
+
+import java.util.Scanner;
+
+/**
+ * Define uma conta de banco que imprime as informações do cliente, receberá
+ * dados via terminal contendo as características de conta em banco conforme
+ * atributos.
+ *
+ * @author EOL
+ * @since 20/03/2025
+ * @version 1.0
+ */
+public class ContaTerminal {
+
+    static int numero;
+    static String agencia;
+    static String nomeCliente;
+    static double saldo;
+    static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = getScannerInstance();
+        nomeCliente = insereNome();
+        agencia = insereAgencia();
+        numero = insereNumero();
+        saldo = insereSaldo();
+        cleanTerminal();
+        printMessage();
+    }
+
+    private static void cleanTerminal() {
+        System.out.print("\033\143");
+    }
+
+    private static void printMessage() {
+        System.out.println("Olá "
+                + nomeCliente
+                + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia
+                + ", conta "
+                + numero
+                + " e seu saldo "
+                + saldo
+                + " já está disponível para saque.");
+    }
+
+    /**
+     * Inicializa um objeto do tipo Scanner
+     *
+     * @return new Scanner()
+     */
+    public static Scanner getScannerInstance() {
+        return new Scanner(System.in);
+    }
+
+    /**
+     * Realiza a entrada do nome do cliente
+     *
+     * @return new Scanner()
+     */
+    private static String insereNome() {
+        System.out.println("Qual o seu nome?");
+        return scanner.nextLine();
+    }
+
+    /**
+     * Realiza a entrada da agencia do cliente
+     *
+     * @return new Scanner()
+     */
+    private static String insereAgencia() {
+        System.out.println("Qual a sua agência?");
+        return scanner.nextLine();
+    }
+
+    /**
+     * Realiza a entrada do numero da conta do cliente
+     *
+     * @return new Scanner()
+     */
+    private static int insereNumero() {
+        System.out.println("Qual o número da conta?");
+        return scanner.nextInt();
+    }
+
+    /**
+     * Realiza a entrada do numero da conta do cliente
+     *
+     * @return new Scanner()
+     */
+    private static double insereSaldo() {
+        System.out.println("Qual o saldo da conta?");
+        return scanner.nextDouble();
+    }
+
+}
